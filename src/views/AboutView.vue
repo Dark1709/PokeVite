022ef7c190b0f2a78502c6d42dc41b47aa6ae983
dark1709 <darly.leon@alternova.com>
2023-07-03 17:52:33 -0500

    Feat: Pokemon app with vue3 and vite.

---
+++ src/views/AboutView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container mt-4 text-body-secondary">
+    <div class="card">
+      <h1 class="text-center">PokeVite</h1>
+      <div class="card-body">
+        <h3>Objetivo:</h3>
+        <p>
+          Realizar una app con vue3 y vite, que pudiera ser urilizada por
+          cualquier miembro de la familia, con la intención de mejorar mis bases
+          en el framework y dejar claros algunos conceptos utilizados, esta app.
+          la seguí con el tutorial de Yuri Marcon de youtube, pero con algunas
+          modificaciones.
+        </p>
+        <h3>Funciones:</h3>
+        <p>
+          Mostrar en pantalla pokemones con su xp y altura, filtrar para
+          encontar a tu favorito y divertirte.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,94 @@
+<script setup>
+import { onMounted, reactive, ref, computed } from "vue";
+import ListPokemons from "../components/ListPokemons.vue";
+import CardPokemonSelected from "../components/CardPokemonSelected.vue";
+
+let urlBaseSvg = ref(
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/"
+);
+let pokemons = reactive(ref());
+let searchPokemonField = ref("");
+let pokemonSelected = reactive(ref());
+let loading = ref(false);
+
+onMounted(() => {
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=649&offset=0")
+    .then((response) => response.json())
+    .then((response) => (pokemons.value = response.results));
+});
+
+const pokemonFiltered = computed(() => {
+  if (pokemons.value && searchPokemonField.value) {
+    return pokemons.value.filter((pokemon) =>
+      pokemon.name
+        .toLowerCase()
+        .includes(searchPokemonField.value.toLowerCase())
+    );
+  }
+  return pokemons.value;
+});
+
+const selectPokemon = async (pokemon) => {
+  loading.value = true;
+  await fetch(pokemon.url)
+    .then((response) => response.json())
+    .then((response) => (pokemonSelected.value = response))
+    .catch((error) => alert(error))
+    .finally(() => (loading.value = false));
+
+  console.log(pokemonSelected.value);
+};
+</script>
+
+<template>
+  <main>
+    <div class="container text-body-secondary">
+      <div class="row mt-4">
+        <div class="col-sm-12 col-md-6">
+          <CardPokemonSelected
+            :name="pokemonSelected?.name"
+            :xp="pokemonSelected?.base_experience"
+            :height="pokemonSelected?.height"
+            :image="pokemonSelected?.sprites.other.dream_world.front_default"
+            :loading="loading"
+          />
+        </div>
+
+        <div class="col-sm-12 col-md-6">
+          <div class="card card-list">
+            <div class="card-body row">
+              <div class="mb-3">
+                <label hidden for="searchPokemonField" class="form-label"
+                  >Buscar...
+                </label>
+
+                <input
+                  v-model="searchPokemonField"
+                  type="text"
+                  class="form-control"
+                  id="searchPokemonField"
+                  placeholder="Buscar..."
+                />
+              </div>
+
+              <ListPokemons
+                v-for="pokemon in pokemonFiltered"
+                :key="pokemon.name"
+                :name="pokemon.name"
+                :urlBaseSvg="urlBaseSvg + pokemon.url.split('/')[6] + '.svg'"
+                @click="selectPokemon(pokemon)"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+<style scoped>
+.card-list {
+  max-height: 75vh;
+  overflow-y: scroll;
+  overflow-x: hidden;
+}
+</style>
